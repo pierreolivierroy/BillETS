3bf5c8fdd225d343bbe2df2468b879fa14fe249f
olivier_rivard <rivard.olivier@gmail.com>
2014-02-22 12:51:36 -0500

    Nouveau Spectacle + nouvelle Représentation

--- src/main/java/ets/gti525/tp2/Representation.java
+++ src/main/java/ets/gti525/tp2/Representation.java
@@ -1,20 +1,50 @@
 package ets.gti525.tp2;
 
-import java.util.ArrayList;
-
 public class Representation {
-	private RepresentationBean representation;
-
-	public Representation(int id, Salle salle, String date, int[] billets, float prix) {
-		representation.setBillets(billets);
-		representation.setDate(date);
-		representation.setId(id);
-		representation.setSalle(salle);
-		representation.setPrix(prix);
+	private int id;
+	private Salle salle;
+	private String date;
+	private int billets;
+	private float prix;
+	
+	public Representation(int id, Salle salle, String date, int billets, float prix) {
+		super();
+		this.id = id;
+		this.salle = salle;
+		this.date = date;
+		this.billets = billets;
+		this.prix = prix;
 	}
-
-	public RepresentationBean getRepresentationBean() {
-		return representation;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Salle getSalle() {
+		return salle;
+	}
+	public void setSalle(Salle salle) {
+		this.salle = salle;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public int getBillets() {
+		return billets;
+	}
+	public void setBillets(int billets) {
+		this.billets = billets;
+	}
+	public float getPrix() {
+		return prix;
+	}
+	public void setPrix(float prix) {
+		this.prix = prix;
 	}
 	
 	
--- src/main/java/ets/gti525/tp2/Spectacle.java
+++ src/main/java/ets/gti525/tp2/Spectacle.java
@@ -1,23 +1,84 @@
 package ets.gti525.tp2;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Spectacle {
 	
-	private SpectacleBean spectacle;
-
-	public Spectacle(int id,String nom,String description, String artistes,String thumbnailPath, String bannerPath,int[] representations ) {
-		this.spectacle = new SpectacleBean();
-		this.spectacle.setArtistes(artistes);
-		this.spectacle.setDescription(description);
-		this.spectacle.setId(id);
-		this.spectacle.setThumbnailPath(thumbnailPath);
-		this.spectacle.setBannerPath(bannerPath);
-		this.spectacle.setNom(nom);
-		this.spectacle.setRepresentations(representations);
+	private int id;
+	private String nom;
+	private String artistes;
+	private HashMap<Integer, Representation> representations;
+	private String description;
+	private String thumbnailPath;
+	private String bannerPath;
+	
+	public Spectacle(int id, String nom, String artistes,
+			HashMap<Integer, Representation> representations, String description, String thumbnailPath,
+			String bannerPath) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.artistes = artistes;
+		this.representations = representations;
+		this.description = description;
+		this.thumbnailPath = thumbnailPath;
+		this.bannerPath = bannerPath;
 	}
 
-	public SpectacleBean getSpectacleBean() {
-		return this.spectacle;
+	public int getId() {
+		return id;
 	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getArtistes() {
+		return artistes;
+	}
+
+	public void setArtistes(String artistes) {
+		this.artistes = artistes;
+	}
+
+	public HashMap<Integer, Representation> getRepresentations() {
+		return representations;
+	}
+
+	public void setRepresentations(HashMap<Integer, Representation> representations) {
+		this.representations = representations;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getThumbnailPath() {
+		return thumbnailPath;
+	}
+
+	public void setThumbnailPath(String thumbnailPath) {
+		this.thumbnailPath = thumbnailPath;
+	}
+
+	public String getBannerPath() {
+		return bannerPath;
+	}
+
+	public void setBannerPath(String bannerPath) {
+		this.bannerPath = bannerPath;
+	}
+	
 }
