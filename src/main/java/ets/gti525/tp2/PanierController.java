fc25de99bb519c7844da3cd85f6aec4f6418a404
Pierre-Olivier Bourgeois <po@bourgeois.me>
2014-03-14 18:33:52 -0400

    Ajout du filtre pour la création du panier dans la session

--- src/main/java/ets/gti525/tp2/PanierController.java
+++ src/main/java/ets/gti525/tp2/PanierController.java
@@ -2,15 +2,12 @@ package ets.gti525.tp2;
 
 import java.math.BigDecimal;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 
 /**
