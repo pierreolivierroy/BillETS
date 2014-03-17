718697fb5e723551dbbc006999675bfbb7ebcb09
dannyboyer <dannyboyer49@gmail.com>
2014-03-16 21:22:09 -0400

    js validation for the cart

---
+++ src/main/webapp/resources/js/panier_empty_validation.js
@@ -0,0 +1,10 @@
+//prevent a custommer to checkout an empty cart
+$(document).ready(function() {
+	check_panier(line_count);
+});
+
+function check_panier(line_count) {
+	if(line_count <= 0) {
+		$("#checkout").addClass("disabled");
+	}
+};
