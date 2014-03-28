f0c5983e8f698a103de3e851827db2b183c3fd7f
pierreolivierroy <roy.pierre.o@gmail.com>
2014-03-28 18:14:37 -0400

    SpectacleDAOPostgre

---
+++ src/main/java/ets/gti525/tp2/SpectacleDAOPostgre.java
@@ -0,0 +1,32 @@
+package ets.gti525.tp2;
+
+import java.util.HashMap;
+
+public class SpectacleDAOPostgre implements ISpectacleDAO {
+
+	@Override
+	public HashMap<Integer, Spectacle> getSpectacles(String nomSpectacle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Spectacle getSpectacle(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashMap<Integer, Representation> getRepresentations(int idSpectacle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Representation getRepresentation(int idSpectacle,
+			int idRepresentation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
