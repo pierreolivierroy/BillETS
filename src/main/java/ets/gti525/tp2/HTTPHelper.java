f421c0249267a2cccfd2047af709a954424881c3
Pierre-Olivier Bourgeois <po@bourgeois.me>
2014-03-28 20:25:16 -0400

    Début de l'implémentation du système de paiement

---
+++ src/main/java/ets/gti525/tp2/HTTPHelper.java
@@ -0,0 +1,69 @@
+package ets.gti525.tp2;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+// La classe est final, car un singleton n'est pas censé avoir d'héritier.
+// En effet, en Java il n'y a pas de polymorphisme sur les méthodes static, il est
+// donc préférable de verrouiller la classe.
+public final class HTTPHelper {
+
+    // L'utilisation du mot clé volatile permet, en Java version 5 et supérieur, 
+    // permet d'éviter le cas où "Singleton.instance" est non-nul,
+    // mais pas encore "réellement" instancié.
+    // De Java version 1.2 à 1.4, il est possible d'utiliser la classe ThreadLocal.
+    private static volatile HTTPHelper instance = null;
+
+    // D'autres attributs, classiques et non "static".
+    private String xxx;
+    private int zzz;
+
+    /**
+     * Constructeur de l'objet.
+     */
+    private HTTPHelper() {
+        // La présence d'un constructeur privé supprime le constructeur public par défaut.
+        // De plus, seul le singleton peut s'instancier lui même.
+        super();
+    }
+
+    /**
+     * Méthode permettant de renvoyer une instance de la classe Singleton
+     * @return Retourne l'instance du singleton.
+     */
+    public final static HTTPHelper getInstance() {
+        //Le "Double-Checked Singleton"/"Singleton doublement vérifié" permet 
+        //d'éviter un appel coûteux à synchronized, 
+        //une fois que l'instanciation est faite.
+        if (HTTPHelper.instance == null) {
+           // Le mot-clé synchronized sur ce bloc empêche toute instanciation
+           // multiple même par différents "threads".
+           // Il est TRES important.
+           synchronized(HTTPHelper.class) {
+             if (HTTPHelper.instance == null) {
+            	 HTTPHelper.instance = new HTTPHelper();
+             }
+           }
+        }
+        return HTTPHelper.instance;
+    }
+
+    public HttpResponse doPost(String hostname, String payload) throws HttpException, IOException, URISyntaxException
+    {
+    	DefaultHttpClient httpClient = new DefaultHttpClient();
+    	HttpPost request = new HttpPost(hostname);
+    	request.addHeader("content-type", "application/json");
+
+    	StringEntity userEntity = new StringEntity(payload);
+    	request.setEntity(userEntity);
+    	
+    	return httpClient.execute(request);
+    }
+
+}
--- src/main/java/ets/gti525/tp2/PreAutorisationPaiement.java
+++ src/main/java/ets/gti525/tp2/PreAutorisationPaiement.java
@@ -1,5 +1,21 @@
 package ets.gti525.tp2;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 import gti525.paiement.IPaiementDAO;
 import gti525.paiement.InformationsPaiementTO;
 import gti525.paiement.ReponseSystemePaiementTO;
@@ -7,28 +23,90 @@ import gti525.paiement.RequeteAuthorisationTO;
 
 public class PreAutorisationPaiement implements IPaiementDAO{
 
+	private Gson gson = new Gson();
+	
 	@Override
 	public ReponseSystemePaiementTO approuverTransaction(
-			RequeteAuthorisationTO arg0) {
+			RequeteAuthorisationTO info_authorisation) {
+		
+		// TODO: Optimisation pour ne pas dupliquer le code pour obtenir la clé d'API
+		Properties props = new Properties();
+		InputStream resources = this.getClass().getClassLoader().getResourceAsStream("api.properties");
+		
+		try {
+			props.load(resources);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String url = (String)props.get("url");
+		String apikey = (String)props.get("apikey");
+		
+		info_authorisation.setStore_id(Integer.parseInt((String)props.get("store_id")));;
+		info_authorisation.setApi_key(apikey);
+		
+		String api_response = null;
+		
+		try {
+			HttpResponse res = HTTPHelper.getInstance().doPost(url + "transactions.json", gson.toJson(info_authorisation));
+			api_response = EntityUtils.toString(res.getEntity());
+			System.out.println(api_response);
+		} catch (HttpException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		// TODO Auto-generated method stub
-		return null;
+		return gson.fromJson(api_response, ReponseSystemePaiementTO.class);
 	}
 
 	@Override
 	public ReponseSystemePaiementTO effectuerPreauthorisation(
 			InformationsPaiementTO info_paiement) {
-		ReponseSystemePaiementTO reponse = new ReponseSystemePaiementTO();
 		
-		//this is just for demo, a cvv code of 999 will result in a faillure
-		if(info_paiement.getSecurity_code() == 999) {
-			//faillure
-			reponse.setCode(0);
+		Properties props = new Properties();
+		InputStream resources = this.getClass().getClassLoader().getResourceAsStream("api.properties");
+		
+		try {
+			props.load(resources);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		else{
-			//success
-			reponse.setCode(1);
-		}	
-		return reponse;
+		
+		String url = (String)props.get("url");
+		String apikey = (String)props.get("apikey");
+		
+		info_paiement.setStore_id(Integer.parseInt((String)props.get("store_id")));
+		info_paiement.setOrder_id(6);
+		info_paiement.setApi_key(apikey);
+		
+		String api_response = null;
+		
+		try {
+			HttpResponse res = HTTPHelper.getInstance().doPost(url + "transactions.json", gson.toJson(new InformationsPaiementTOAPI(info_paiement)));
+			api_response = EntityUtils.toString(res.getEntity());
+			
+		} catch (HttpException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// TODO: Supprimer le root element, parce que le parsing marche pas.
+		return gson.fromJson(api_response, ReponseSystemePaiementTO.class);
 	}
 
 }
